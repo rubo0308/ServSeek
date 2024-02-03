ad56ecad6b469450d1588da1d99ee8d4685a9dd7
Ruben <rub.hakobyan2007@gmail.com>
2024-02-03 18:48:34 +0400

    added phone number verification

---
+++ app/src/main/java/com/example/servseek/LoginOtpActivity.java
@@ -0,0 +1,18 @@
+package com.example.servseek;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class LoginOtpActivity extends AppCompatActivity {
+    String phoneNumber;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_otp);
+        phoneNumber = getIntent().getExtras().getString("phone");
+        Toast.makeText(getApplicationContext(), phoneNumber, Toast.LENGTH_LONG).show();
+    }
+}
