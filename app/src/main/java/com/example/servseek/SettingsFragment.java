1e4e76b61f551c6b6d3607fa71f95d8670f0a014
Ruben <rub.hakobyan2007@gmail.com>
2024-02-28 00:35:04 +0400

    s ndjkssssss

--- app/src/main/java/com/example/servseek/SettingsFragment.java
+++ app/src/main/java/com/example/servseek/SettingsFragment.java
@@ -1,5 +1,6 @@
 package com.example.servseek;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +8,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.servseek.utils.FirebaseUtil;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class SettingsFragment extends Fragment {
+    TextView logoutButton;
 
 
     public SettingsFragment() {
@@ -17,10 +23,25 @@ public class SettingsFragment extends Fragment {
 
 
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        logoutButton = view.findViewById(R.id.logout_button);
+
+        logoutButton.setOnClickListener((v) -> FirebaseMessaging.getInstance().deleteToken().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                FirebaseUtil.logout();
+                Intent intent = new Intent(getContext(), SplashActivity.class); // Adjust as per your activity
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        }));
+
+        return view;
     }
+
 }
