d28a1e9fb7a586a74c396eb92890776990999679
rubo0308 <rub.hakobyan2007@gmail.com>
2024-03-01 22:01:36 +0400

    do notifications on and of

--- app/src/main/java/com/example/servseek/NotificationsActivity.java
+++ app/src/main/java/com/example/servseek/NotificationsActivity.java
@@ -3,17 +3,32 @@ package com.example.servseek;
 
 
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NotificationsActivity extends AppCompatActivity {
+    private TextView labelNotificationStatus;
+    private Switch switchNotification;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifications);
+
+        labelNotificationStatus = findViewById(R.id.label_notification_status);
+        switchNotification = findViewById(R.id.switch1);
+        updateNotificationStatusText(switchNotification.isChecked());
+
+
+        // Set the initial text based on the switch state
+
 
         ImageButton backButton = findViewById(R.id.back_btn);
         backButton.setOnClickListener(new View.OnClickListener() {
@@ -23,5 +38,17 @@ public class NotificationsActivity extends AppCompatActivity {
                 finish();
             }
         });
+        switchNotification.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                updateNotificationStatusText(isChecked);
+            }
+        });
     }
-}
+
+    private void updateNotificationStatusText(boolean isOn) {
+        labelNotificationStatus.setText(isOn ? "Notification is on" : "Notification is off");
+    }
+
+    }
+
---
+++ app/src/main/java/com/example/servseek/adapter/ArrayList.java
@@ -0,0 +1,4 @@
+package com.example.servseek.adapter;
+
+public class ArrayList<T> {
+}
